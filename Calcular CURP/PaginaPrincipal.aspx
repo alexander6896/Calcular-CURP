0c8657c4bf5b87c8fd26e95094bad3c2ee782699
alexander6896 <alexander6896@hotmail.com>
2020-05-27 21:42:25 -0500

    Primer Commit, donde se crea la primer parte de la interfaz

--- Calcular CURP/PaginaPrincipal.aspx
+++ Calcular CURP/PaginaPrincipal.aspx
@@ -4,13 +4,19 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
             <asp:MultiView ID="MultiView1" runat="server">
+                <asp:View ID="View1" runat="server">
+                </asp:View>
+                <asp:View ID="View1" runat="server">
+                </asp:View>
+                <asp:View ID="View1" runat="server">
+                </asp:View>
                 <asp:View ID="View1" runat="server">
                 </asp:View>
             </asp:MultiView>
--- Calcular CURP/PaginaPrincipal.aspx.designer.cs
+++ Calcular CURP/PaginaPrincipal.aspx.designer.cs
@@ -40,5 +40,32 @@ namespace Calcular_CURP
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.View View1;
+
+        /// <summary>
+        /// Control View1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View View1;
+
+        /// <summary>
+        /// Control View1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View View1;
+
+        /// <summary>
+        /// Control View1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View View1;
     }
 }
